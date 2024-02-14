d4f233fbda926b50ea8cc82c01487c7b15dc2767
Note-salaf <123761829+Note-salaf@users.noreply.github.com>
2024-02-14 06:44:39 -0300

    Add files via upload
    Jogo para trabalhar a alfabetização palavras com g ou gu.

---
+++ script.js
@@ -0,0 +1,67 @@
+const imagem = document.querySelector('#imagem')
+//imagem.src = 'imagens/água.jpg'
+const palavra = document.querySelector('.palavra')
+//palavra.textContent = 'ok'
+const resposta1 = document.querySelector('.resposta1')
+//resposta1.textContent = 'ok'
+const resposta2 = document.querySelector('.resposta2')
+//resposta2.textContent = 'ok'
+let i = 0
+const btnProxima = document.querySelector('.btnProxima')
+//btnProxima.textContent = 'ok'
+const mensagem  = document.querySelector('.mensagem')
+//mensagem.textContent = 'ok'
+
+
+const conjImagens = ['imagens/água.jpg', 'imagens/régua.jpg', 'imagens/bigode.png']
+const conjPalavras = ['Á___A', 'RÉ___A', 'BI___ODE']
+const conjRespostas = [['G', 'GU']]
+
+
+imagem.src = conjImagens[i]
+palavra.textContent = conjPalavras[i]
+
+function proxima() {
+    //btnProxima.textContent = 'ok'
+   if (i <= conjImagens.length - 2) {
+      i++
+      imagem.src = conjImagens[i]
+      palavra.textContent = conjPalavras[i]
+   } else {
+    i=0
+    imagem.src = conjImagens[i]
+    palavra.textContent = conjPalavras[i]
+   }
+   mensagem.textContent = 'Clique na resposta correta!'
+}
+
+function confere() {
+switch (i) {
+    case 0:
+        if (this.textContent == 'GU') {
+            mensagem.textContent = 'Parabéns, você acertou!'
+        } else {
+            mensagem.textContent = 'Tente novamente!'
+        }
+        break;
+    case 1:
+        if (this.textContent == 'GU') {
+            mensagem.textContent = 'Parabéns, você acertou!'
+        } else {
+            mensagem.textContent = 'Tente novamente!'
+        }
+        break;
+    case 2:
+        if (this.textContent == 'G') {
+            mensagem.textContent = 'Parabéns, você acertou!'
+        } else {
+            mensagem.textContent = 'Tente novamente!'
+        }
+        break;
+    default:
+        break;
+}
+}
+btnProxima.addEventListener('click', proxima)
+resposta1.addEventListener('click', confere)
+resposta2.addEventListener('click', confere)
